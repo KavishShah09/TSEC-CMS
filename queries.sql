bd26668a90ac9842e0838576864a797058ed9f48
Kavish Shah <kavishshah09@gmail.com>
2020-05-08 18:21:14 +0530

    fixed queries.sql

--- queries.sql
+++ queries.sql
@@ -1,27 +1,38 @@
-CREATE TABLE `cms`;
+CREATE DATABASE `cms`;
 USE `cms`;
 
 CREATE TABLE `complaints`
 (
   `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) DEFAULT NULL,
-  `author` varchar(100) DEFAULT NULL,
+  `title` varchar
+(255) DEFAULT NULL,
+  `author` varchar
+(100) DEFAULT NULL,
   `body` text,
   `issue_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` varchar(10) DEFAULT 'Open',
-  PRIMARY KEY(`id`)
+  `status` varchar
+(10) DEFAULT 'Open',
+  PRIMARY KEY
+(`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`
 (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `year` varchar(4) DEFAULT NULL,
-  `username` varchar(100) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
+  `name` varchar
+(100) DEFAULT NULL,
+  `email` varchar
+(100) DEFAULT NULL,
+  `year` varchar
+(4) DEFAULT NULL,
+  `username` varchar
+(100) DEFAULT NULL,
+  `password` varchar
+(255) DEFAULT NULL,
   `register_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `role` varchar(6) DEFAULT NULL,
-  PRIMARY KEY(`id`)
+  `role` varchar
+(6) DEFAULT NULL,
+  PRIMARY KEY
+(`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
